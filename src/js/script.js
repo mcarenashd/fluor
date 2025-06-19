98450f75503381459ab5622d97bc42dd8d4da36b
Camila Arenas <mcarenasdh@gmail.com>
2025-06-19 22:09:29 +0200

    Add JS elements

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const circulo = document.getElementById("circulo1");
+
+  circulo.addEventListener("click", () => {
+    circulo.setAttribute("fill", "red");
+  });
+  console.log("¡Estoy viva!")
+
+
+  const svg = document.getElementById("mySvg");
+
+  svg.addEventListener("mouseenter", () => {
+    svg.pauseAnimations();
+  });
+
+  svg.addEventListener("mouseleave", () => {
+    svg.unpauseAnimations();
+  });
