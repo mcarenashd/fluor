d9b4a7ac9f08b4136e4444a6374ebf6b14092275
olgararo <olgararo1402@gmail.com>
2025-06-22 21:48:56 +0200

    Update quiz

--- src/js/historia.js
+++ src/js/historia.js
@@ -6,3 +6,27 @@ document.addEventListener('DOMContentLoaded', function() {
         // Opcionalmente mostrar un botón de play
     });
 });
+
+
+//QUIZ
+
+function checkAnswer(button) {
+  const questionDiv = button.parentElement;
+  const selected = questionDiv.querySelector('input[type="radio"]:checked');
+  const resultDiv = questionDiv.querySelector('.result');
+  const correct = questionDiv.dataset.answer;
+
+  if (!selected) {
+    resultDiv.textContent = "Selecciona una respuesta.";
+    resultDiv.style.color = "#cc0000";
+    return;
+  }
+
+  if (selected.value === correct) {
+    resultDiv.textContent = "¡Correcto!";
+    resultDiv.style.color = "#006600";
+  } else {
+    resultDiv.textContent = "Incorrecto 😬";
+    resultDiv.style.color = "#cc0000";
+  }
+}
