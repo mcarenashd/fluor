ed93fb6d3df57453414ca19bfcea5a3a4c71df35
Camila Arenas <mcarenasdh@gmail.com>
2025-06-24 15:29:40 +0200

    add sweetAlert2

--- src/js/script.js
+++ src/js/script.js
@@ -36,22 +36,107 @@ if (atomContainer) {
 
 const proton9 = document.getElementById("proton9");
 
-function mostrarMensaje () {
-Swal.fire({
-  title: "¿Quién soy?",
-  text: 'El flúor es el elemento químico más electronegativo: atrae más fuerte los electrones que cualquier otro.',
-  width: 600,
-  padding: "1em",
-  color: "#86A694",
-  background: "#fff url(/images/trees.png)",
-  backdrop: `
-    rgba(0, 0, 123, 0.2)
-    url("/images/nyan-cat.gif")
-    left top
-    no-repeat
-  `
-});
+function mostrarMensaje1 () {
+        Swal.fire({
+            title: "¿Datos científicos interesantes?",
+            html: `<ul style="text-align: left; margin: 0 auto; display: inline-block;">
+                <li>🔥 Soy el elemento más electronegativo de todos.</li>
+                <li>💎 A temperatura ambiente soy un gas tóxico y amarillo.</li>
+                <li>⚗️ Puedo reaccionar hasta con gases nobles como el xenón.</li>
+                <li>💥 El flúor puro es tan reactivo que casi nada sobrevive a mi contacto.</li>
+                <li>🔗 Formo enlaces súper fuertes en la química orgánica.</li>
+            </ul>
+        `,
+            imageUrl: "src/images/img-index/fluor-gif1.gif",
+            confirmButtonText: "Home",
+            confirmButtonColor: "#f9d369",
+            background: "#FDF6E2",
+            
+            color: "#592851",
+            customClass: {
+              popup: 'swal-custom-popup',
+              confirmButton: 'my-custom-confirm',
+              image: 'swal-custom-image',
+            },
+            backdrop: `
+          rgba(0,0,123,0.4)
+             url("/images/nyan-cat.gif")
+            left top
+            no-repeat
+            `
+         });
 }
 
-proton9.addEventListener("click", function(){mostrarMensaje()})
+proton9.addEventListener("click", function(){mostrarMensaje1()})
 
+const proton8 = document.getElementById("proton8");
+
+function mostrarMensaje () {
+        Swal.fire({
+            title: "Comparaciones y contexto",
+            html: `<ul style="text-align: left; margin: 0 auto; display: inline-block;">
+                <li>🌈 “Soy un halógeno como el cloro y el yodo.</li>
+                <li>🔬 Estoy en el grupo 17 de la tabla periódica..</li>
+                <li>🔢 Mi número atómico es 9.</li>
+                <li>🎯 Con solo 1 electrón más, alcanzo la estabilidad.</li>
+            </ul>
+        `,
+            imageUrl: "src/images/img-index/fluor-gif2.gif",
+            confirmButtonText: "Home",
+            confirmButtonColor: "#f9d369",
+            background: "#FDF6E2",   
+            color: "#592851",
+            customClass: {
+              popup: 'swal-custom-popup',
+              confirmButton: 'my-custom-confirm',
+              image: 'swal-custom-image' 
+            },
+            backdrop: `
+          rgba(0,0,123,0.4)
+             url("/images/nyan-cat.gif")
+            left top
+            no-repeat
+            `
+         });
+}
+
+proton8.addEventListener("click", function(){mostrarMensaje()})
+
+function mostrarMensaje2 () {
+        Swal.fire({
+            title: "¿Quién soy?",
+            text: 'El flúor puro es un gas amarillo pálido, tóxico y corrosivo. ¡Debe manejarse con mucho cuidado!',
+            imageUrl: "src/images/img-index/fluor-gif2.gif",
+            confirmButtonText: "Home",
+            confirmButtonColor: "#f9d369",
+            background: "#FDF6E2",   
+            color: "#592851",
+            customClass: {
+              popup: 'swal-custom-popup',
+              confirmButton: 'my-custom-confirm',
+              image: 'swal-custom-image' 
+            },
+            backdrop: `
+          rgba(0,0,123,0.4)
+             url("/images/nyan-cat.gif")
+            left top
+            no-repeat
+            `
+         });
+}
+
+proton7.addEventListener("click", function(){mostrarMensaje2()})
+
+        // Swal.fire({
+        //     title: "¿Quién soy?",
+        //     text: "El flúor es el elemento químico más electronegativo: atrae más fuerte los electrones que cualquier otro.",
+        //     icon: "success",
+        //     confirmButtonText: "¡Increíble!",
+        //     confirmButtonColor: "#f9d369",
+        //     background: "#FDF6E2",
+        //     color: "#592851",
+        //     customClass: {
+        //       popup: 'swal-custom-popup',
+        //       confirmButton: 'my-custom-confirm'
+        //     }
+        // });
