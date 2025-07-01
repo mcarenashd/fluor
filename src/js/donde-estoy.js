6453380c376db55663288d515eb98465d15fcd92
MarianaMH1195 <morenomariana03@gmail.com>
2025-07-01 16:34:17 +0200

    responsive adjustement

--- src/js/donde-estoy.js
+++ src/js/donde-estoy.js
@@ -1,33 +1,3 @@
-/* const boton = document.getElementById("boton");
-
-function mostrarMensaje() {
-  Swal.fire({
-    title: "La fluorita brilla en la oscuridad 💎✨",
-    html: `El mineral fluorita, del cual se obtiene el flúor, puede brillar bajo luz ultravioleta. De hecho, ¡la palabra “fluorescencia” viene de “fluorita”!
-            
-        `,
-    imageUrl: "../src/images/img-donde-estoy/GIFFLUOR.gif",
-    confirmButtonText: "Home",
-    confirmButtonColor: "#f9d369",
-    background: "#000",
-    color: "#black",
-    
-    customClass: {
-      popup: 'swal-custom-popup',
-      confirmButton: 'my-custom-confirm',
-      image: 'swal-custom-image'
-    },
-    backdrop: `
-          rgba(0, 0, 0, 0.85)
-             url("/images/nyan-cat.gif")
-            left top
-            no-repeat
-            `
-  });
-}
-
-boton.addEventListener("click", function () { mostrarMensaje() }) */
-
 const boton = document.getElementById("boton");
 
 // Crear los objetos de audio con Howler.js
